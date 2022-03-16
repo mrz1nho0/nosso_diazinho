3a1b0a45bf0370ef7daad02aa6aef91ec51a8b4d
Miguel Ryan <101756906+mrz1nho0@users.noreply.github.com>
2022-03-16 19:50:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function textin() {
+    window.open("textin.html", "_self");
+}
+
+function musiquinha() {
+    window.open("musica.html", "_self");
+}
+
+function momentinhos() {
+    window.open("momentos.html", "_self");
+}
